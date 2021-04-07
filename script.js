9dfc68e46526e7147552a10e34000c4b04cd20ac
Paulina <paulinah09@gmail.com>
2021-04-07 07:55:39 -0500

    removed syntax errors by alerts, updated preventDefault block to effect the form NOT the submit button itself, better organized code for readability

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 // Write your JavaScript code here!
 window.addEventListener("load", function() {
-   document.getElementById("formSubmit").addEventListener("click", function(event){
+   let userInputForm = document.querySelector("form");
+   userInputForm.addEventListener("submit", function(event){
       event.preventDefault();
-      console.log("did this trigger")});
-   let form = document.querySelector("form");
-   form.addEventListener("submit", function(event) {
+      console.log("did this trigger")
+   });
+   
+   userInputForm.addEventListener("submit", function(event) {
       let pilotName = document.querySelector("input[name=pilotName]");
       let copilotName = document.querySelector("input[name=copilotName]");
       let fuelLevel = document.querySelector("input[name=fuelLevel]");
@@ -12,10 +14,10 @@ window.addEventListener("load", function() {
       if (pilotName.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass.value === "") {
          alert("All fields are required!");
       }
-      if (isNaN(pilotName.value) = fallse || (isNaN(copilotName.value) = false) {
+      if (isNaN(pilotName.value) === false || isNaN(copilotName.value) === false) {
          alert("Make sure to enter valid information for each field.");
       }
-      if (isNaN(fuelLevel.value) = true || isNaN(cargoMass.value) = true) {
+      if (isNaN(fuelLevel.value) === true || isNaN(cargoMass.value) === true) {
          alert("Make sure to enter valid information for each field.");
       }
    });
