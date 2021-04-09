b729f81c6ad08aaac80e49c86643fd6f0f8e4165
Paulina <paulinah09@gmail.com>
2021-04-08 20:14:31 -0500

    reworked placement of functions and declared variables. fixed the logical of the cargo mass/fuel level functions to make it one function. got css updating for launch status message

--- script.js
+++ script.js
@@ -3,10 +3,7 @@ window.addEventListener("load", function() {
    let userInputForm = document.querySelector("form");
    userInputForm.addEventListener("submit", function(event){
       event.preventDefault();
-      console.log("did this trigger")
-   });
-   
-   userInputForm.addEventListener("submit", function(event) {
+      console.log("did this trigger");
       let pilotName = document.querySelector("input[name=pilotName]");
       let copilotName = document.querySelector("input[name=copilotName]");
       let fuelLevel = document.querySelector("input[name=fuelLevel]");
@@ -20,53 +17,57 @@ window.addEventListener("load", function() {
       if (isNaN(fuelLevel.value) === true || isNaN(cargoMass.value) === true) {
          alert("Make sure to enter valid information for each field.");
       }
+      
+      let userProvidedPilotName = document.querySelector("input[name=pilotName]").value;
+      let userProvidedCopilotName = document.querySelector("input[name=copilotName]").value;
+      let userProvidedFuelLevel = document.querySelector("input[name=fuelLevel]").value;
+      let userProvidedCargoMass = document.querySelector("input[name=cargoMass]").value;
+   
+      
+      updateCrewNames(userProvidedPilotName, userProvidedCopilotName);
+      updateFuelLevelAndCargoMassStatus(userProvidedFuelLevel, userProvidedCargoMass);
+   
+
    });
    
-   let userProvidedPilotName = document.querySelector("input[name=pilotName]");
-   let userProvidedCopilotName = document.querySelector("input[name=copilotName]");
-   let userProvidedFuelLevel = document.querySelector("input[name=fuelLevel]");
-   let userProvidedCargoMass = document.querySelector("input[name=cargoMass]");
-   
-   updateCrewNames(userProvidedPilotName, userProvidedCopilotName);
-   updateFuelLevelStatus(userProvidedFuelLevel);
-   updateCargoMassStatus(userProvidedCargoMass);
 
 });
 
 
 function updateCrewNames(userProvidedPilotName, userProvidedCopilotName) {
-   // let userProvidedPilotName = document.querySelector("input[name=pilotName]");
-   // let userProvidedCopilotName = document.querySelector("input[name=copilotName]");
-   document.getElementById("pilotStatus").innerHTML = "Pilot" + userProvidedPilotName + "is ready for launch";
-   document.getElementById("copilotStatus").innerHTML = "Co-pilot" + userProvidedCopilotName + "is ready for launch";
+   document.getElementById("pilotStatus").innerHTML = `Pilot ${userProvidedPilotName} is ready for launch`;
+   document.getElementById("copilotStatus").innerHTML = `Co-pilot ${userProvidedCopilotName} is ready for launch`;
 }
 
-function updateFuelLevelStatus(userProvidedFuelLevel) {
-   // let userProvidedFuelLevel = document.querySelector("input[name=fuelLevel]");
-   if (userProvidedFuelLevel >= 10000) {  
-      document.getElementById("fuelStatus").innerHTML = userProvidedFuelLevel + "Fuel level is high enough for launch";
-      document.getElementById("launchStatus").innerHTML = "Shuttle is ready for launch";
-      document.getElementById("launchStatus").style.color = "green";
-   } else {
+function updateFuelLevelAndCargoMassStatus(userProvidedFuelLevel, userProvidedCargoMass) {
+   let launchStatusMessage = document.getElementById("launchStatus");
+   console.log(userProvidedFuelLevel);
+   if (userProvidedFuelLevel >= 10000 && userProvidedCargoMass <= 10000) {  
+      document.getElementById("fuelStatus").innerHTML = `${userProvidedFuelLevel} Fuel level is high enough for launch`;
+      document.getElementById("cargoStatus").innerHTML = `${userProvidedCargoMass} Cargo mass is low enough for launch`;
+      launchStatusMessage.innerHTML = "Shuttle is ready for launch";
+      launchStatusMessage.style.color = "green";
+   } else if (userProvidedFuelLevel < 10000 && userProvidedCargoMass > 10000) {
       document.getElementById("faultyItems").style.visibility = "visible";
-      document.getElementById("fuelStatus").innerHTML = userProvidedFuelLevel + "Fuel level is too low for launch";
-      document.getElementById("launchStatus").innerHTML = "Shuttle not ready for launch";
-      document.getElementById("launchStatus").style.color = "red";
-   } 
+      document.getElementById("fuelStatus").innerHTML = `${userProvidedFuelLevel} Fuel level is too low for launch`;
+      document.getElementById("cargoStatus").innerHTML = `${userProvidedCargoMass} Cargo mass is too high for launch`;
+      launchStatusMessage.innerHTML = "Shuttle not ready for launch";
+      launchStatusMessage.style.color = "red";
+   } else if (userProvidedFuelLevel < 10000) {
+      document.getElementById("faultyItems").style.visibility = "visible";
+      document.getElementById("fuelStatus").innerHTML = `${userProvidedFuelLevel} Fuel level is too low for launch`;
+      document.getElementById("cargoStatus").innerHTML = `${userProvidedCargoMass} Cargo mass is low enough for launch`;
+      launchStatusMessage.innerHTML = "Shuttle not ready for launch";
+      launchStatusMessage.style.color = "red";
+   }  else if (userProvidedCargoMass > 10000) {
+      document.getElementById("faultyItems").style.visibility = "visible";
+      document.getElementById("fuelStatus").innerHTML = `${userProvidedFuelLevel} Fuel level is high enough for launch`;
+      document.getElementById("cargoStatus").innerHTML = `${userProvidedCargoMass} Cargo mass is too high for launch`;
+      launchStatusMessage.innerHTML = "Shuttle not ready for launch";
+      launchStatusMessage.style.color = "red";
+
 }
 
-function updateCargoMassStatus(userProvidedCargoMass) {
-   // let userProvidedCargoMass = document.querySelector("input[name=cargoMass]");
-   if (userProvidedCargoMass <= 10000) {
-      document.getElementById("cargoStatus").innerHTML = userProvidedCargoMass + "Cargo mass is low enough for launch";
-      document.getElementById("launchStatus").innerHTML = "Shuttle is ready for launch";
-      document.getElementById("launchStatus").style.color = "green";
-   } else {
-      document.getElementById("faultyItems").style.visibility = "visible";
-      document.getElementById("cargoStatus").innerHTML = userProvidedCargoMass + "Cargo mass is too high for launch";
-      document.getElementById("launchStatus").innerHTML = "Shuttle not ready for launch";
-      document.getElementById("launchStatus").style.color = "red";
-   }
 }
 
 
